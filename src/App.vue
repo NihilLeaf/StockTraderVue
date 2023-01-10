c50bb32abb4b8276b6af9e04902dcaed6fbf71e2
NihilLeaf <nihilleaf@gmail.com>
2023-01-10 08:55:33 -0300

    Components criados

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Stock Trader</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'Main_Content',
   components: {
-    HelloWorld
-  }
-}
+  },
+  data: () => ({
+  }),
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    font-family: 'Sofia Sans', sans-serif;
+  }
 </style>
